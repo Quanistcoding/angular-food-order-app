03d6dd2056dadc6555a690693ed4edc4f2fede30
Chihhung Jen <quanistcoding@gmail.com>
2023-02-05 11:57:00 +0800

    Added routes and corresponding components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ManageOrdersComponent } from './manage-orders/manage-orders.component';
+import { ManageProductsComponent } from './manage-products/manage-products.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'my-orders', component: MyOrdersComponent },
+  { path: 'admin/order', component: ManageOrdersComponent },
+  { path: 'admin/products', component: ManageProductsComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { LoginComponent } from './login/login.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { ManageOrdersComponent } from './manage-orders/manage-orders.component';
+import { ManageProductsComponent } from './manage-products/manage-products.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ShoppingCartComponent,
+    LoginComponent,
+    MyOrdersComponent,
+    ManageOrdersComponent,
+    ManageProductsComponent,
+    NotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
