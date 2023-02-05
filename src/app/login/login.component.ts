3e482312bb8b48822140755323cec02ba5233a23
Chihhung Jen <quanistcoding@gmail.com>
2023-02-05 13:37:05 +0800

    Added functionality to return user to their intent page after log in.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
-  constructor(private authService: AuthService) {}
+export class LoginComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.authService.auth.user.subscribe((user) => {
+      if (user) {
+        const { returnUrl } = this.route.snapshot.queryParams;
+        if (returnUrl) this.router.navigate([returnUrl]);
+        else this.router.navigate(['/']);
+      }
+    });
+  }
+
   login() {
     this.authService.login();
   }
--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
 import { AuthService } from './auth.service';
 import firebase from 'firebase/compat/app';
 @Injectable({
@@ -7,14 +12,17 @@ import firebase from 'firebase/compat/app';
 })
 export class AuthGuardService implements CanActivate {
   user?: firebase.User | null;
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate() {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     this.authService.auth.user.subscribe((user) => {
       this.user = user;
     });
 
-    if (this.user) return true;
+    if (this.user) {
+      return true;
+    }
+    this.router.navigate(['login'], { queryParams: { returnUrl: state.url } });
     return false;
   }
 }
