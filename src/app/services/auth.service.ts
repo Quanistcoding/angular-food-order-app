905be16d5dc7656548f1948f39f82be995236bb4
Chihhung Jen <quanistcoding@gmail.com>
2023-02-07 22:43:35 +0800

    Moved adding user after login logic to from app.component to auth service.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
+import { UserService } from './user.service';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(public auth: AngularFireAuth) {}
+  constructor(public auth: AngularFireAuth, private userService: UserService) {}
   login() {
-    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.auth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then((response) => {
+        const user = response.user;
+        if (!user) return;
+        const userData = {
+          name: user.displayName,
+          email: user.email,
+          phone: user.phoneNumber,
+        };
+
+        this.userService.findOneAndUpdate(user.uid, userData);
+
+        console.log(response.user);
+      });
   }
 
   logout() {
