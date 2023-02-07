6fe326e767167e9ffab674f0094fc75055a57639
Chihhung Jen <quanistcoding@gmail.com>
2023-02-07 23:43:39 +0800

    Added listing product categories on the save product page.

--- src/app/save-product/save-product.component.ts
+++ src/app/save-product/save-product.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CategoryService } from '../services/category.service';
 import { ProductService } from '../services/product.service';
-import { take } from 'rxjs';
+import { Observable, take } from 'rxjs';
+import { Product } from '../models/product.model';
 @Component({
   selector: 'app-save-products',
   templateUrl: './save-product.component.html',
   styleUrls: ['./save-product.component.scss'],
 })
 export class SaveProductComponent implements OnInit {
-  categories$: any;
   product: any = {};
   id?: string | null;
+  categories: any = [];
+
   constructor(
     private categoryService: CategoryService,
     private productService: ProductService,
@@ -20,16 +22,24 @@ export class SaveProductComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.categories$ = this.categoryService
+    this.categoryService
       .getAll()
-      .valueChanges({ idField: 'id' });
+      .valueChanges({ idField: 'id' })
+      .pipe(take(1))
+      .subscribe((c) => {
+        this.categories = c;
+      });
+
     this.id = this.route.snapshot.paramMap.get('id');
+
     if (this.id)
       this.productService
         .findOne(this.id)
         .valueChanges()
         .pipe(take(1))
-        .subscribe((p) => (this.product = p));
+        .subscribe((p) => {
+          this.product = p;
+        });
   }
 
   save(product: any) {
